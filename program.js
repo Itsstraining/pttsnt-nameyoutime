35b747e7633ff9b9a9749f1b6a088a060db52bc8
DiepThienThien <bananakind123@gmail.com>
2021-01-27 19:19:58 +0700

    sua lan 1

--- program.js
+++ program.js
@@ -1,8 +1,41 @@
 // Add new functions, variables here
+function Tach(x) {
+  let kq = [];
+  let tl;
+  let temp = [];
+
+  for (let i = 2; i < x - 1; i++) {
+    if (x % i == 0) {
+      temp.push(i);
+    }
+  }
+  for (let i = 0; i < temp.length; i++) {
+    if (temp[i] * temp[i + 1] == x) {
+      return (kq = kq + temp[i] + " " + temp[i + 1]);
+    }
+    return kq;
+  }
+
+  function kt(x) {}
+
+  // for (let i = 0; i < temp.length; i++) {
+  //   if (!kq.includes(temp[i])) {
+  //     kq.push[temp[i]];
+  //   }
+  // }
+  // return kq;
+}
 
 function main(input) {
   // Your code goes here
   // Using console.log to answer the question
+
+  let x = parseInt(input);
+  if (x == 0) {
+    console.log(0);
+  } else {
+    console.log(Tach(x));
+  }
 }
 
 module.exports = main;
